5b43abd10f9cbdb2fc3c5e8c30416b4b77678fb6
Katarzyna <meerkatsan@gmail.com>
2017-07-29 21:18:40 +0200

    new files

---
+++ App.js
@@ -0,0 +1,38 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  	X-Client-Id: 2052
+	X-Auth-Token: d9bb3e6cb430e4e93e26cad4650a1d67
+};
+$.ajaxSetup({
+	headers: myHeaders
+});
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
+// OGÓLNA FUNKCJA
+// function randomString() {
+// 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+// 	var str = '', i;
+// 	for (i = 0; i < 10; i++) {
+// 	  str += chars[Math.floor(Math.random() * chars.length)];
+// 	}
+// 	return str;
+// }
+
